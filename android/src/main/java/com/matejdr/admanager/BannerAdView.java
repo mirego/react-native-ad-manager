7b427de14416024c2c863f092ce23d3415876d0e
Raphaël Blanchet <rblanchet@mirego.com>
2023-09-28 11:48:49 -0400

    Add missing import

--- android/src/main/java/com/matejdr/admanager/BannerAdView.java
+++ android/src/main/java/com/matejdr/admanager/BannerAdView.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
